b18d58abd4e78474b66ad0481233bf717dfbfb8f
TurbulentCupcake <muraliadithya315@gmail.com>
2018-04-24 17:54:11 -0500

    NN test 2 passing

--- hw10/Neural.java
+++ hw10/Neural.java
@@ -13,7 +13,10 @@ public class Neural {
         return 1 / (1 + Math.exp(-x));
     }
     
+    //  RETURN VALUE FOR ALL NEURONS IS OF THE FORM {Ui,Vi}, 
+    //  FOR OUTPUT NEURON, Vi is the output we want
 
+    // Unit A neuron
     private static double [] unitA(Double[] weights, Double inputx1, Double inputx2) {
 
         double u1 = weights[0]*1 + weights[1]*inputx1 + weights[2]*inputx2;
@@ -23,6 +26,7 @@ public class Neural {
 
     }
 
+    // Unit B neuron
     private static double [] unitB(Double[] weights, Double inputx1, Double inputx2) {
         
         double u2 = weights[3]*1 + weights[4]*inputx1 + weights[5]*inputx2;
@@ -31,6 +35,7 @@ public class Neural {
         return results;
     }
 
+    // Unit C neuron
     private static double [] unitC(Double[] weights, Double inputx1, Double inputx2) {
         
         double u3 = weights[6]*1 + weights[7]*inputx1 + weights[8]*inputx2;
@@ -39,25 +44,38 @@ public class Neural {
         return results;
     }
 
+
+    private static double calculateError(Double vc, Double y) {
+        return 0.5*Math.pow(vc - y,2);
+    }
+
+    private static double partialDerivativeVc(Double vc, Double y) {
+        return (vc - y);
+    }
+
+    private static double partialDerivativeUc(Double vc, Double y) {
+        return (vc - y)*vc*(1 - vc);
+    }
+
     public static void main(String args[]) {
         
         int option = Integer.parseInt(args[0]);
         
-        
+
         
         if(option == 100) {
-            // parse the weight inputs
+
             for(int i = 1 ; i < args.length -2 ; i++) {
                 weights[i-1] = Double.parseDouble(args[i]); 
             }
-
+    
             x1 = Double.parseDouble(args[args.length-2]);
             x2 = Double.parseDouble(args[args.length-1]);
-
+            
             double [] r1 = unitA(weights, x1, x2);
             double [] r2 = unitB(weights, x1, x2);
             double [] r3 = unitC(weights, r1[1] , r2[1]);
-
+            
             System.out.println(String.format("%.5f ",r1[0]) +
                                String.format("%.5f ",r1[1]) + 
                                String.format("%.5f ",r2[0])  + 
@@ -70,7 +88,21 @@ public class Neural {
             
         } else if(option == 200) {
 
-            
+            for(int i = 1 ; i < 10 ; i++) {
+                weights[i-1] = Double.parseDouble(args[i]); 
+            }
+
+            x1 = Double.parseDouble(args[10]);
+            x2 = Double.parseDouble(args[11]);
+            Double y = Double.parseDouble(args[12]);
+
+            double [] r1 = unitA(weights, x1, x2);
+            double [] r2 = unitB(weights, x1, x2);
+            double [] r3 = unitC(weights, r1[1] , r2[1]);
+
+            System.out.println(String.format("%.5f ", calculateError(r3[1], y)) + 
+                               String.format("%.5f ", partialDerivativeVc(r3[1], y)) + 
+                               String.format("%.5f ", partialDerivativeUc(r3[1], y)));
 
         }
 

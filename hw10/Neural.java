0f6e8a135b0c0b7c7ae4eb49f3dd9f43cbd400fe
TurbulentCupcake <muraliadithya315@gmail.com>
2018-04-24 19:18:11 -0500

    test 3 works

--- hw10/Neural.java
+++ hw10/Neural.java
@@ -45,7 +45,8 @@ public class Neural {
     }
 
 
-    private static double calculateError(Double vc, Double y) {
+    // The following functions calculate errors and derivatives with respect to the output unit C
+    private static double calculateErrorVc(Double vc, Double y) {
         return 0.5*Math.pow(vc - y,2);
     }
 
@@ -56,6 +57,41 @@ public class Neural {
     private static double partialDerivativeUc(Double vc, Double y) {
         return (vc - y)*vc*(1 - vc);
     }
+
+    // the following functions calculate partial derivatives with respect to the hidden layer units A and B
+
+    private static double partialDerivativeVj(Double [] weights, Double vc, Double y, char unit) {
+        if(unit == 'A') {    
+            double result = weights[7]*partialDerivativeUc(vc, y);
+            return result;
+        } else if (unit == 'B') {
+            double result = weights[8]*partialDerivativeUc(vc, y);
+            return result;
+        }
+        return 0.0;
+    }
+
+    private static double partialDerivativeUj(Double [] weights, Double vc, Double y, Double inputx1, Double inputx2, char unit) {
+
+        double result = 0;
+        if(unit == 'A') {
+            if(unitA(weights, inputx1, inputx2)[0] >= 0) {
+                result = partialDerivativeVj(weights, vc, y, unit)*1;
+            } else {
+                result = partialDerivativeVj(weights, vc, y, unit)*0;
+            }
+            return result;
+        } else if (unit == 'B') {
+            if(unitB(weights, inputx1, inputx2)[0] >= 0) {
+                result = partialDerivativeVj(weights, vc, y, unit)*1;
+            } else {
+                result = partialDerivativeVj(weights, vc, y, unit)*0;
+            }
+            return result;
+        }
+        return result;
+    }
+
 
     public static void main(String args[]) {
         
@@ -100,10 +136,31 @@ public class Neural {
             double [] r2 = unitB(weights, x1, x2);
             double [] r3 = unitC(weights, r1[1] , r2[1]);
 
-            System.out.println(String.format("%.5f ", calculateError(r3[1], y)) + 
+            System.out.println(String.format("%.5f ", calculateErrorVc(r3[1], y)) + 
                                String.format("%.5f ", partialDerivativeVc(r3[1], y)) + 
                                String.format("%.5f ", partialDerivativeUc(r3[1], y)));
 
+        } else if (option == 300) {
+
+            for(int i = 1 ; i < 10 ; i++) {
+                weights[i-1] = Double.parseDouble(args[i]); 
+            }
+
+            x1 = Double.parseDouble(args[10]);
+            x2 = Double.parseDouble(args[11]);
+            Double y = Double.parseDouble(args[12]);
+            
+            double [] r1 = unitA(weights, x1, x2);
+            double [] r2 = unitB(weights, x1, x2);
+            double [] r3 = unitC(weights, r1[1] , r2[1]);
+
+            System.out.println(String.format("%.5f ", partialDerivativeVj(weights, r3[1], y, 'A')) + 
+                               String.format("%.5f ", partialDerivativeUj(weights, r3[1], y, x1, x2, 'A')) + 
+                               String.format("%.5f ", partialDerivativeVj(weights, r3[1], y, 'B')) + 
+                               String.format("%.5f ", partialDerivativeUj(weights, r3[1], y, x1, x2, 'B')));
+
+
+            
         }
 
 
